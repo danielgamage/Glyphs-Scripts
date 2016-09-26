699c0d8dd505adb53563a4930c3826afdb6b2bff
Daniel Gamage <hello@danielgamage.com>
2016-09-26 18:18:18 -0400

    add continue, rollback scripts & add documentation

--- Selections/Continue selection.py
+++ Selections/Continue selection.py
@@ -1,1 +1,21 @@
-# insp: modo can make checkered selections
+#MenuTitle: Continue Selection
+lastNode = Layer.selection[len(Layer.selection) - 1]
+originNode = Layer.selection[len(Layer.selection) - 2]
+
+# Get difference of two nodes
+if lastNode.index > originNode.index:
+    # normal diff
+    rhythm = lastNode.index - originNode.index
+else:
+    # crossing bounds of path
+    rhythm = abs(originNode.index - len(originNode.parent.nodes)) + lastNode.index
+
+# Move to node width rhythm
+nodeToSelect = lastNode
+i = 0
+while i < rhythm:
+    nodeToSelect = nodeToSelect.nextNode
+    i += 1
+
+# Select it
+nodeToSelect.selected = True
---
+++ Selections/Rollback selection.py
@@ -0,0 +1,7 @@
+#MenuTitle: Rollback Selection
+
+# Get last-selected node
+lastNode = Layer.selection[len(Layer.selection) - 1]
+
+# Deselect it
+lastNode.selection = False
