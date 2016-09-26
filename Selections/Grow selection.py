d7c446b3aca102fc0408246f3a3cf6d0bdcc84e8
Daniel Gamage <hello@danielgamage.com>
2016-09-26 15:54:28 -0400

    fixin up all operators lol

--- Selections/Grow selection.py
+++ Selections/Grow selection.py
@@ -2,7 +2,7 @@
 nodesToSelect = []
 for path in Layer.paths:
     for node in path.nodes:
-        if node.selected != True:
+        if not node.selected:
             if node.nextNode.selected or node.prevNode.selected:
 				nodesToSelect.append(node)
 for node in nodesToSelect:
