87af0b6df906c14702bab5287b7e74c562083df1
Daniel Gamage <hello@danielgamage.com>
2016-09-26 15:53:28 -0400

    fixing up my operators

--- Selections/Grow selection.py
+++ Selections/Grow selection.py
@@ -3,8 +3,7 @@ nodesToSelect = []
 for path in Layer.paths:
     for node in path.nodes:
         if node.selected != True:
-            if (node.nextNode.selected | node.prevNode.selected):
-				print node
+            if node.nextNode.selected or node.prevNode.selected:
 				nodesToSelect.append(node)
 for node in nodesToSelect:
 	node.selected = True
--- Selections/Shrink selection.py
+++ Selections/Shrink selection.py
@@ -3,8 +3,7 @@ nodesToDeselect = []
 for path in Layer.paths:
     for node in path.nodes:
         if node.selected:
-            if (node.nextNode.selected ^ node.prevNode.selected):
-				print node
+            if not node.nextNode.selected or not node.prevNode.selected:
 				nodesToDeselect.append(node)
 for node in nodesToDeselect:
 	node.selected = False
