664ce3ee00e8c681907079aa0ea351682f6163bd
Daniel Gamage <hello@danielgamage.com>
2016-10-18 18:05:55 -0400

    added convert to offcurves script

---
+++ Nodes/Convert to Offcurves.py
@@ -0,0 +1,8 @@
+#MenuTitle: Convert selected nodes to offcurves
+# -*- coding: utf-8 -*-
+__doc__ = """
+This is a rather dangerous operation.
+Usually used on all nodes of a path to make a closed quad curve.
+"""
+for node in Layer.selection:
+    node.type = "offcurve"
