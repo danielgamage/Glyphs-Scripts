f4a8da2b59cea4106870938548da617221024c7f
Daniel Gamage <hello@danielgamage.com>
2016-10-19 13:23:38 -0400

    fixed add midpoint

--- Subdivision/Add Midpoint.py
+++ Subdivision/Add Midpoint.py
@@ -6,5 +6,6 @@ Adds a midpoint between selected nodes.
 
 for node in Layer.selection:
     if (node.nextNode.selected):
+        path = node.parent
         nodeIndex = path.indexOfNode_(node)
         path.insertNodeWithPathTime_(nodeIndex + 1.5)
