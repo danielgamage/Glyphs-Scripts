6bada5c867cea13eef02567f6474f8ad8ea00c12
Daniel Gamage <hello@danielgamage.com>
2016-10-22 18:25:38 -0400

    remove unused vars

--- Subdivision/Subdivide to curves.py
+++ Subdivision/Subdivide to curves.py
@@ -12,15 +12,12 @@ for path in Layer.paths:
     # store static node count
     nodeCount = len(path.nodes)
 
-    corners = list(path.nodes)
-    newPoints = []
-
     for i in xrange(nodeCount, 0, -1):
-    	# insert nodes backwards
+        # insert nodes backwards
         p1 = path.insertNodeWithPathTime_(i + ONE_THIRD) if i == nodeCount else path.insertNodeWithPathTime_(i + 0.5)
         p2 = path.insertNodeWithPathTime_(i - ONE_THIRD) if i > 1 else path.insertNodeWithPathTime_(i - 0.5)
 
-		# find midpoints
+        # find midpoints
         midpointX = (p1.x + p2.x) / 2
         midpointY = (p1.y + p2.y) / 2
         midpoint = Cocoa.NSMakePoint(midpointX, midpointY)
@@ -29,8 +26,8 @@ for path in Layer.paths:
         node.setPosition_(midpoint)
 
     for i in xrange(0, nodeCount):
-    	oncurveNode = path.nodes[i * 3].nextNode
-    	oncurveNode.type = "curve"
-    	oncurveNode.connection = "smooth"
-    	oncurveNode.prevNode.type = "offcurve"
-    	oncurveNode.nextNode.type = "offcurve"
+        oncurveNode = path.nodes[i * 3].nextNode
+        oncurveNode.type = "curve"
+        oncurveNode.connection = "smooth"
+        oncurveNode.prevNode.type = "offcurve"
+        oncurveNode.nextNode.type = "offcurve"
