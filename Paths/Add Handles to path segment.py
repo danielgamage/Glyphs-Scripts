6eef0f9478a0563925a02bd4809bfa906e05336b
Daniel Gamage <hello@danielgamage.com>
2016-12-17 16:28:57 -0500

    fixed handle adding on closed paths

--- Paths/Add Handles to path segment.py
+++ Paths/Add Handles to path segment.py
@@ -1,14 +1,15 @@
-#MenuTitle: Add handles to selected path segments
+#MenuTitle: Add handles to selected segments
 # -*- coding: utf-8 -*-
 __doc__ = """
 Adds offcurve points to selected straight line path segments.
 """
 
 for node in Layer.selection:
-  path = node.parent
-  if node.nextNode.selected and node.index != len(path.nodes) - 1 and node.type != 'offcurve' and node.nextNode.type != 'offcurve':
-  	offcurve1 = path.insertNodeWithPathTime_(node.index + 1.6667)
-  	offcurve2 = path.insertNodeWithPathTime_(node.index + 1.5)
-  	offcurve1.type = 'offcurve'
-  	offcurve2.type = 'offcurve'
-  	node.nextNode.nextNode.nextNode.type = 'curve'
+    path = node.parent
+    if node.nextNode.selected and node.type != 'offcurve' and node.nextNode.type != 'offcurve':
+        if not (not path.closed and node.index == len(path.nodes) - 1):
+            offcurve1 = path.insertNodeWithPathTime_(node.index + 1.6667)
+            offcurve2 = path.insertNodeWithPathTime_(node.index + 1.5)
+            offcurve1.type = 'offcurve'
+            offcurve2.type = 'offcurve'
+            node.nextNode.nextNode.nextNode.type = 'curve'
