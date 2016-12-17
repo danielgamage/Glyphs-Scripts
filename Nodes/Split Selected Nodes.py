6777c0e93c1317ed5369b409838d292fcd56d594
Daniel Gamage <hello@danielgamage.com>
2016-12-17 15:27:53 -0500

    much better inktrap creation

--- Nodes/Split Selected Nodes.py
+++ Nodes/Split Selected Nodes.py
@@ -1,4 +1,4 @@
-#MenuTitle: Split Selected Nodes
+#MenuTitle: Split Selected Nodes (Make Inktraps)
 # -*- coding: utf-8 -*-
 __doc__ = """
 Splits nodes into two and separates them by 2 units at 90°-snapped angle.
@@ -9,18 +9,25 @@ import Cocoa
 import math
 import copy
 
-
 def translateNode(node, distance):
-    angleFloat = node.parent.tangentAngleAtNode_direction_(node, 1) + 90
-    angleSnapped = round(angleFloat / 90) * 90 * (math.pi/180)
-    x = node.x + distance * math.cos(angleSnapped)
-    y = node.y + distance * math.sin(angleSnapped)
+    path = node.parent
+    angleFloat = path.tangentAngleAtNode_direction_(node, path.direction) + (90 * path.direction)
+    angleSnapped = round(angleFloat / 90) * 90
+    # always make corners LESS acute and separate away from vertex
+    direction = -1 if angleFloat < angleSnapped else 1
+    # convert to radians for translation
+    angleSnappedRadians = angleSnapped * (math.pi/180)
+
+    x = node.x + distance * direction * math.cos(angleSnappedRadians)
+    y = node.y + distance * direction * math.sin(angleSnappedRadians)
     newPoint = Cocoa.NSMakePoint(x, y)
     return newPoint
 
 for node in Layer.selection:
     path = node.parent
     cloneNode = node.copy()
+
+    # make sure nodes don't go in the same direction
     originPosition = translateNode(node, 1)
     clonePosition = translateNode(node, -1)
 
